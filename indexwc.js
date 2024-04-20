b2569cfd64470f6ca2f8647792adce4a5a36baec
focus2learn <160470982+focus2learn@users.noreply.github.com>
2024-04-20 10:04:02 +0500

    updated word count file
    included repeat looping condition

--- indexwc.js
+++ indexwc.js
@@ -4,29 +4,41 @@
 // import packages
 import inquirer from "inquirer";
 import chalk from "chalk";
-// Assign Counter Variable Count without white spaces
-let cwows = 0;
-console.clear();
-// Assign Constant Object variable to get input from user
-const wordcount = await inquirer.prompt({
-    name: "sentence",
-    type: "input",
-    message: chalk.blue("\n\n\t\t Type your Text to Count Words...."),
-});
-// Assigne Constant variable to store manipulated / processed data
-let wordcount2 = wordcount.sentence.trim().split(" ");
-// loop for , to count Array words's Characters
-for (let i = 0; i < wordcount2.length; i++) {
-    cwows = cwows + wordcount2[i].length;
+// Assign variable for a while looping condition
+let condition = true;
+while (condition) {
+    // Assign Counter Variable Count without white spaces
+    let cwows = 0;
+    console.clear();
+    // Assign Constant Object variable to get input from user
+    const wordcount = await inquirer.prompt({
+        name: "sentence",
+        type: "input",
+        message: chalk.blue("\n\n\t\t Type your Text to Count Words...."),
+    });
+    // Assigne Constant variable to store manipulated / processed data
+    let wordcount2 = wordcount.sentence.trim().split(" ");
+    // loop for , to count Array words's Characters
+    for (let i = 0; i < wordcount2.length; i++) {
+        cwows = cwows + wordcount2[i].length;
+    }
+    // Display Original Text Data whatever User given Input
+    console.clear();
+    console.log(chalk.yellow("\n\n\n\t\t------------------------ WORD COUNT-------------------------"));
+    console.log(chalk.green("\n\n\t\tOriginal Text........ :  ") + chalk.yellow(wordcount.sentence));
+    // Display Trimed and Splited data from user input
+    console.log(chalk.green("\n\n\t\tText as Array........ :  ") + chalk.yellow(wordcount2));
+    console.log(chalk.green("\n\n\t\tWord(s) Count........ :  ") + chalk.yellow(wordcount2.length));
+    console.log(chalk.green("\n\n\t\tCharacter(s) Count... :  ") + chalk.yellow(wordcount.sentence.split("").length) + "   With White-Spaces");
+    console.log(chalk.green("\n\n\t\tCharacter(s) Count... :  ") + chalk.yellow(cwows) + "   Without White-Spaces");
+    console.log(chalk.yellow("\n\n\n\t\t------------------------ focus2learn-------------------------"));
+    // get input to re-enter new sentence
+    const repeat = await inquirer.prompt({
+        name: "domore",
+        type: "confirm",
+        message: chalk.rgb(100, 200, 20)("\n\t\tDo you want to type new sentence....."),
+        default: false,
+    });
+    condition = repeat.domore;
 }
-// Display Original Text Data whatever User given Input
-console.clear();
-console.log(chalk.yellow("\n\n\n\t\t------------------------ WORD COUNT-------------------------"));
-console.log(chalk.green("\n\n\t\tOriginal Text........ :  ") + chalk.yellow(wordcount.sentence));
-// Display Trimed and Splited data from user input
-console.log(chalk.green("\n\n\t\tText as Array........ :  ") + chalk.yellow(wordcount2));
-console.log(chalk.green("\n\n\t\tWord(s) Count........ :  ") + chalk.yellow(wordcount2.length));
-console.log(chalk.green("\n\n\t\tCharacter(s) Count... :  ") + chalk.yellow(wordcount.sentence.split("").length) + "   With White-Spaces");
-console.log(chalk.green("\n\n\t\tCharacter(s) Count... :  ") + chalk.yellow(cwows) + "   Without White-Spaces");
-console.log(chalk.yellow("\n\n\n\t\t------------------------ focus2learn-------------------------"));
 // End of coding......... focus2learn ...............
